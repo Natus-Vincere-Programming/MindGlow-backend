76eb3f2142b9303a015b581216f38eaa2bd187b4
Maks Balamut <balamutmaks@gmail.com>
2024-05-08 22:35:02 +0300

    Додано базовий проект Spring Boot, Spring Data JPA, Spring Web

---
+++ src/main/java/com/natusvincere/mindblow/MindBlowApplication.java
@@ -0,0 +1,13 @@
+package com.natusvincere.mindblow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindBlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindBlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natusvincere/mindblow/MindBlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natusvincere.mindblow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindBlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
